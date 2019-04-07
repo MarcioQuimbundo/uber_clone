ca68d670070dcb3ae230c22311ed52c80519c122
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-04-07 12:22:00 +0100

    save

--- uber_rider/test/widget_test.dart
+++ uber_rider/test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:uber_rider/main.dart';
+import 'package:uber_rider/src/pages/login.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(LoginPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
