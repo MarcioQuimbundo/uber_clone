ba668bf4e3338ec2fe559150e66a49a4f700b5cb
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-04-20 19:41:53 +0100

    bottomNavigation Added

--- uber_driver/lib/ui/home.dart
+++ uber_driver/lib/ui/home.dart
@@ -78,6 +78,24 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Container(
+        height: 90,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey, blurRadius: 11, offset: Offset(3.0, 4.0))
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(padding: EdgeInsets.only(left: 20),child: Icon(Icons.keyboard_arrow_up)),
+            Text("You're offline", style: TextStyle(fontSize: 30,)),
+            Container(padding: EdgeInsets.only(right: 20),child: Icon(Icons.list)),
+          ],
+        ),
+      ),
       resizeToAvoidBottomInset: false,
       key: _scaffoldKey,
       bottomSheet: Container(
@@ -100,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Align(
               alignment: Alignment.topCenter,
               child: Container(
-                margin: EdgeInsets.only(top: 20),
+                margin: EdgeInsets.only(top: 30),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -121,26 +139,29 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
           Positioned(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  FunctionalButton(
-                    icon: Icons.security,
-                    title: "",
-                    onPressed: () {},
-                  ),
-                  GoButton(
-                    title: "GO",
-                    onPressed: () {},
-                  ),
-                  Container(
-                    width: 50,
-                    height: 50,
-                  )
-                ],
+            child: Container(
+              margin: EdgeInsets.only(bottom: 20),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    FunctionalButton(
+                      icon: Icons.security,
+                      title: "",
+                      onPressed: () {},
+                    ),
+                    GoButton(
+                      title: "GO",
+                      onPressed: () {},
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                ),
               ),
             ),
           )
@@ -192,8 +213,7 @@ class _FunctionalButtonState extends State<FunctionalButton> {
 class ProfileWidget extends StatefulWidget {
   final Function() onPressed;
 
-  const ProfileWidget({Key key, this.onPressed})
-      : super(key: key);
+  const ProfileWidget({Key key, this.onPressed}) : super(key: key);
 
   @override
   _ProfileWidgetState createState() => _ProfileWidgetState();
@@ -206,16 +226,19 @@ class _ProfileWidgetState extends State<ProfileWidget> {
       decoration: BoxDecoration(
         border: Border.all(color: Colors.white, width: 4),
         shape: BoxShape.circle,
-        boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 11, offset: Offset(3.0, 4.0))],
+        boxShadow: [
+          BoxShadow(
+              color: Colors.grey, blurRadius: 11, offset: Offset(3.0, 4.0))
+        ],
       ),
       child: ClipOval(
-                  child: Image.asset(
-                    "assets/images/user_profile.jpg",
-                    width: 60,
-                    height: 60,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+        child: Image.asset(
+          "assets/images/user_profile.jpg",
+          width: 60,
+          height: 60,
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
@@ -240,16 +263,25 @@ class _PriceWidgetState extends State<PriceWidget> {
         border: Border.all(color: Colors.white, width: 4),
         color: Colors.black,
         borderRadius: BorderRadius.all(Radius.circular(50.0)),
-        boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 11, offset: Offset(3.0, 4.0))],
+        boxShadow: [
+          BoxShadow(
+              color: Colors.grey, blurRadius: 11, offset: Offset(3.0, 4.0))
+        ],
       ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text("\$",
-              style: TextStyle(color: Colors.green, fontSize: 26,fontWeight: FontWeight.bold)),
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold)),
           Text(widget.price,
-              style: TextStyle(color: Colors.white, fontSize: 26,fontWeight: FontWeight.bold)),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold)),
         ],
       ),
     );
