45dccdbd2d50d51ea2e59e7b3846701e8c155ca3
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-04-22 18:37:50 +0100

    refactoring the code

--- uber_rider/lib/src/ui/home.dart
+++ uber_rider/lib/src/ui/home.dart
@@ -2,6 +2,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uber_rider/src/ui/widgets/functionalButton.dart';
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -354,52 +355,6 @@ class _MyHomePageState extends State<MyHomePage> {
     points.add(_createLatLng(-8.914580, 13.202106));
 
     return points;
-  }
-}
-
-class FunctionalButton extends StatefulWidget {
-  final String title;
-  final IconData icon;
-  final Function() onPressed;
-
-  const FunctionalButton({Key key, this.title, this.icon, this.onPressed})
-      : super(key: key);
-
-  @override
-  _FunctionalButtonState createState() => _FunctionalButtonState();
-}
-
-class _FunctionalButtonState extends State<FunctionalButton> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        RawMaterialButton(
-          onPressed: widget.onPressed,
-          splashColor: Colors.black,
-          fillColor: Colors.white,
-          elevation: 15.0,
-          shape: CircleBorder(),
-          child: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Icon(
-                widget.icon,
-                size: 30.0,
-                color: Colors.black,
-              )),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 2.0),
-          child: Text(
-            widget.title,
-            style: TextStyle(fontSize: 15.0, color: Colors.black),
-          ),
-        )
-      ],
-    );
   }
 }
 
---
+++ uber_rider/lib/src/ui/widgets/functionalButton.dart
@@ -0,0 +1,47 @@
+import "package:flutter/material.dart";
+
+class FunctionalButton extends StatefulWidget {
+  final String title;
+  final IconData icon;
+  final Function() onPressed;
+
+  const FunctionalButton({Key key, this.title, this.icon, this.onPressed})
+      : super(key: key);
+
+  @override
+  _FunctionalButtonState createState() => _FunctionalButtonState();
+}
+
+class _FunctionalButtonState extends State<FunctionalButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        RawMaterialButton(
+          onPressed: widget.onPressed,
+          splashColor: Colors.black,
+          fillColor: Colors.white,
+          elevation: 15.0,
+          shape: CircleBorder(),
+          child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Icon(
+                widget.icon,
+                size: 30.0,
+                color: Colors.black,
+              )),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 2.0),
+          child: Text(
+            widget.title,
+            style: TextStyle(fontSize: 15.0, color: Colors.black),
+          ),
+        )
+      ],
+    );
+  }
+}
