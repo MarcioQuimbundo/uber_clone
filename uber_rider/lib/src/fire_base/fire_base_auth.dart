1d865721138f117354721ef775c6baafa97cc560
Marcio Teixeira Quimbundo [DIGITAL FACTORY] <marcioquimbundo@hotmail.com>
2019-07-23 17:38:42 +0100

    _onSignupError function added

--- uber_rider/lib/src/fire_base/fire_base_auth.dart
+++ uber_rider/lib/src/fire_base/fire_base_auth.dart
@@ -4,29 +4,49 @@ import 'package:firebase_database/firebase_database.dart';
 class FireAuth {
   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  void signUp(String email, String pass, String name, String phone, Function onSuccess) {
+  void signUp(String email, String pass, String name, String phone, Function onSuccess, Function(String) onRegisterError) {
     _firebaseAuth
         .createUserWithEmailAndPassword(email: email, password: pass)
         .then((user) {
       //
-      _createUser(user.uid, name, phone, onSuccess);
+      _createUser(user.uid, name, phone, onSuccess, onRegisterError);
       print(user);
     }).catchError((err) {
       // TODO
     });
   }
 
-  _createUser(String userId, String name, String phone, Function onSuccess) {
+  _createUser(String userId, String name, String phone, Function onSuccess, Function(String) onRegisterError) {
     var user = {
       "name": name,
       "phone": phone,
     };
     var ref = FirebaseDatabase.instance.reference().child("users");
+
     ref.child(userId).set(user).then((user) {
       // success
       onSuccess();
     }).catchError((err) {
       //TODO
+      onRegisterError("Signup Fail, please try again");
     });
+  }
+
+  void _onSignupError(String code, Function(String) onRegisterError) {
+    switch (code) {
+      case "ERROR_INVALID_EMAIL":
+      case "ERROR_INVALID_CREDENTIAL":
+        onRegisterError("Invalid Email");
+        break;
+      case "ERROR_EMAIL_ALREADY_IN_USE":
+        onRegisterError("Email has existed");
+        break;
+      case "ERROR_WEAK_PASSWORD":
+        onRegisterError("The password is not strong enough");
+        break;
+      default:
+        onRegisterError("Signup fail, please try again");
+        break;
+    }
   }
 }
